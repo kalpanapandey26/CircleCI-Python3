5d1dca2f6d5f74b46aeae6ed791615357f1f641a
kalpanapandey26 <kalpanapp26@gmail.com>
2025-09-10 10:26:32 +0530

    test

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 def to_upper(name):
     return name.upper()
@@ -11,17 +10,3 @@ if __name__ == '__main__':
     say_hello(name)
     up = to_upper(name)
     print(up)
-=======
-
-def to_upper(name):
-    return name.upper()
-
-def say_hello(name):
-   return print(f'Hello, {name}')
-
-if __name__ == '__main__':
-    name = "TrainWithTannu"
-    say_hello(name)
-    up = to_upper(name)
-    print(up)
->>>>>>> 1d03134ac0c7e8f955cbffe1e4e0b3c417f4b65b
